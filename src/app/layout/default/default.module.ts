54e27b2bb99d7e4bdfc42330c5c4571e6fcdaef6
Kai Richardson <kairichardson2000@gmail.com>
2020-04-30 18:51:29 -0700

    added card mod

--- src/app/layout/default/default.module.ts
+++ src/app/layout/default/default.module.ts
@@ -7,6 +7,8 @@ import { PostsComponent } from 'src/app/modules/posts/posts.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatCardModule } from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [DefaultComponent, DashboardComponent, PostsComponent],
@@ -16,6 +18,8 @@ import { MatDividerModule } from '@angular/material/divider';
     SharedModule,
     MatSidenavModule,
     MatDividerModule,
+    FlexLayoutModule,
+    MatCardModule,
   ],
 })
 export class DefaultModule {}
