0e2c1ad45a6da4a35629bd828af2122fa7fc4c66
Kai Richardson <kairichardson2000@gmail.com>
2020-04-30 18:05:43 -0700

    fixing

--- src/app/shared/widgets/area/area.component.ts
+++ src/app/shared/widgets/area/area.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as Highcharts from 'highcharts';
+import HC_exporting from 'highcharts/modules/exporting';
 
 @Component({
   selector: 'app-widget-area',
@@ -19,42 +20,21 @@ export class AreaComponent implements OnInit {
         type: 'area',
       },
       title: {
-        text: 'Historic and Estimated Worldwide Population Growth by Region',
+        text: 'Random DATA',
       },
       subtitle: {
-        text: 'Source: Wikipedia.org',
+        text: 'Demo',
       },
-      xAxis: {
-        categories: ['1750', '1800', '1850', '1900', '1950', '1999', '2050'],
-        tickmarkPlacement: 'on',
-        title: {
-          enabled: false,
-        },
-      },
-      yAxis: {
-        title: {
-          text: 'Billions',
-        },
-        labels: {
-          formatter: function () {
-            return this.value / 1000;
-          },
-        },
-      },
+
       tooltip: {
         split: true,
         valueSuffix: ' millions',
       },
-      plotOptions: {
-        area: {
-          stacking: 'normal',
-          lineColor: '#666666',
-          lineWidth: 1,
-          marker: {
-            lineWidth: 1,
-            lineColor: '#666666',
-          },
-        },
+      credits: {
+        enabled: false,
+      },
+      exporting: {
+        enabled: true,
       },
       series: [
         {
@@ -79,5 +59,11 @@ export class AreaComponent implements OnInit {
         },
       ],
     };
+
+    HC_exporting(Highcharts);
+
+    setTimeout(() => {
+      window.dispatchEvent(new Event('resize'));
+    }, 300);
   }
 }
