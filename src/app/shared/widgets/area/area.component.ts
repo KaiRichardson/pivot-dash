c66e63c4e9665731ff526d9c0633dca10f7d82f0
Kai Richardson <kairichardson2000@gmail.com>
2020-05-16 20:14:11 -0700

    table fill

--- src/app/shared/widgets/area/area.component.ts
+++ src/app/shared/widgets/area/area.component.ts
@@ -2,6 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import * as Highcharts from 'highcharts';
 import HC_exporting from 'highcharts/modules/exporting';
 
+var agents = [
+  {
+    name: 'Kai Richardson',
+    data: [15.3, 14.8, 14.8, 15, 15, 0, 0, 14.5, 15, 15.2, 15.3, 14.2, 0, 0],
+  },
+  {
+    name: 'Erin Richardson',
+    data: [17.3, 15.8, 16.8, 15.2, 16, 0, 0, 16.5, 17, 16.2, 16.3, 16.2, 0, 0],
+  },
+];
+
 @Component({
   selector: 'app-widget-area',
   templateUrl: './area.component.html',
