6a3f380a1a1779a0614b8f96b25e58a60f7adf56
Kai Richardson <kairichardson2000@gmail.com>
2020-06-12 20:39:54 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 //Install express server
 const bodyParser = require("body-parser");
 const express = require("express");
-const events = require("./events");
+const events = require("./controllers/agents");
+const connection = require("./config/connection");
 const cors = require("cors");
 const path = require("path");
 
-const app = express();
+// const app = express();
 
-connection.connect();
+// connection.connect();
+
+// Start the app by listening on the default Heroku port
+const port = process.env.PORT || 8080;
+
+const app = express()
+  .use(cors())
+  .use(bodyParser.json())
+  .use(events(connection));
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + "/dist/pivot-dash"));
@@ -16,18 +25,10 @@ app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname + "/dist/pivot-dash/index.html"));
 });
 
-// Start the app by listening on the default Heroku port
-const port = process.env.PORT || 8080;
-
 // Import routes and give the server access to them.
-var routes = require("./controllers/burgersController.js");
+// var routes = require("./controllers/burgersController.js");
 
-app.use(routes);
-
-const app = express()
-  .use(cors())
-  .use(bodyParser.json())
-  .use(events(connection));
+// app.use(routes);
 
 app.listen(port, () => {
   console.log(`Express server listening on port ${port}`);
