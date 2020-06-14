3f90a3ddd7801ea5e5904c656fc81f13b0685aa1
Kai Richardson <kairichardson2000@gmail.com>
2020-06-14 14:08:39 -0700

    sucsessful refactor of angular

---
+++ server/src/index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mysql = require("mysql");
+const bearerToken = require("express-bearer-token");
+const events = require("./events");
+const oktaAuth = require("./auth");
+
+// const connection;
+
+// if (process.env.JAWSDB_URL) {
+//   // Database is JawsDB on Heroku
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//   // Database is local
+//   connection = mysql.createConnection({
+//     port: 3306,
+//     host: "m7nj9dclezfq7ax1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+//     user: "ci6x184naqtb2so7",
+//     password: "k85ihpgsneu16rpj",
+//     database: "lcuqs5ux986qociq",
+//   });
+// }
+
+const connection = mysql.createConnection({
+  port: 3306,
+  host: "m7nj9dclezfq7ax1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  user: "ci6x184naqtb2so7",
+  password: "k85ihpgsneu16rpj",
+  database: "lcuqs5ux986qociq",
+});
+
+connection.connect();
+
+const port = process.env.PORT || 8080;
+
+const app = express()
+  .use(cors())
+  .use(bodyParser.json())
+  .use(bearerToken())
+  .use(oktaAuth)
+  .use(events(connection));
+
+app.listen(port, () => {
+  console.log(`Express server listening on port ${port}`);
+});
